8d0a5aab56433858f1cb18a17572a3d8bafab43c
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-04-28 00:24:26 +0000

    details page completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,11 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list" })
   end 
+
+  def show
+    render({ :template => "director_templates/details" })
+  end
+  
+  
+
 end  
